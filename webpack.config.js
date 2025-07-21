ccea8551a06032125b847e85878323068583e9a3
Harris Gordon <harrisgordon@Harriss-MacBook-Pro.local>
2025-07-21 18:01:18 -0400

    feat: implement webpack for frontend build process

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './static/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'static', 'dist'),
+  },
+  mode: 'production',
+}; 
